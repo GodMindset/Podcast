7417f5140814fbb1f85893f743c1de176ee2e098
Andrei Lucaci <lucaciandrei@outlook.com>
2020-09-03 13:41:45 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-body">
+        <Typography variant="h2">Welcome to God Mindset</Typography>
+        <Typography variant="caption">
+          This website is under construction
+        </Typography>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/sheets/sheet.js
@@ -0,0 +1,14 @@
+export const googleSheetConfiguration = {
+  type: "service_account",
+  project_id: "godmindsetpodcast2020",
+  private_key_id: "201df9ddc24e4cf1b2f53ac6fec4e22ea1757a8c",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCOpc1+uEgUKoYz\nR/Yo38EZGAD+Z0DRBKAM/7sk/FTnuwYTGtqV4zKHeLrYWLjRkEc+DEtvABYa4EWL\nCCpFqc9No6wvoPqvRYaCeM4fcM+B3bqxSHcqNE0xZlpDcOJVwbXzf/u9mXG7P6vd\nT9uvpk8t8z18nwpOXsPaAAeDc19c25vjxWlO82GJh+fQvQy65ALiliWREFKpJ/cI\nG7aqoXn0iY6wYoJMduaBAtqoVjbqO43qtS4lsd0KkPnKsJg1cTZWx1jvtawm/6Je\n//m6dNRzZ2GX/kK3HVzcx72zjcYWaHRCLiRIRk1deaf4zpApbIDmOx3wt88hNXke\nOC0F+hHpAgMBAAECggEAI2LTDs/lJYeFAfykfnA/RRvUx0dS52b30zGvOZYU8+VL\ntPqQ5eTV+JBE/X1I38ekPKqtHcyOLz2aUsqEPgLJZ6+l+6y58zfSjdQtAdSozUT1\nVpws333Y9lu3IYrDwtVKbyy9BMQnV/WMC1sPmsutUa9I/fywAE/20sH2GzBIJE8B\nTQ72M8krPOBS+/VqR0oLbQUrWVWceFUzr5KeTmtG0hTCpMO6sOlZRhZeskNf44Ph\nmKtJQapYTirSw3iV8GLvGYd+9UrGzHPpy2vzjzoV1wKGrowoDlnw2TeMiFpdReiU\nuGY0vvJ23eI/bzSMplIoCjnm+p1NrWicQ3qsltBdPwKBgQDEVbHk+8mN9cd54vIr\nyNCxwYznLweQVKaaMqy4avNiEm/T/tB6NnQF/fzUn2K17kA23PMhYF1FT5s/ifPN\njljdc9b/lDo/z/gamd3BPMYiniiN1gc8RSe6k58iaynuoU1XLNLyHGHXs95obauj\na1kkYVHeRg62gDrT5rULi1vM9wKBgQC5/2Z1OFFHdqCdmuVuewKvZRlk1AIyI7pD\nObf/BxHeIoa/Rob2YnXS+IOtQvPdFnieOjxu952UG8CHYQEsK/V4AJaR9DJVgv2T\nGjFcfiD/fl2j5RENedMxaWzLDzc+zkn5MH0epK5dfG191G/fakB0NDHwhinT6Ex2\nPk7PyzXAHwKBgQC3NdB/gYYf9s5iKykZOpekiJzXzsccJCIjmk4a+ZKMXfAEv7AD\nm3TBcrXQI7uVUfKSP7SR3L6swxB4Rl8/3imG3WYvsT559bPr/tE0VWX/P99QG/08\nfLwKG19QWvgSOmtVgfqG+31CMBLHGDAmmzHgBQDEjlOEcjxzKMKhNzY/GQKBgBpa\nq+PUFQAxONTcC8I5v8mmY5C70YvVMCEyXnXORKPa9IjJr3ndvaAlAlNcdz1OX8zb\np5B5mWRRAlisUAQ4PlHAk5UeqrsfElrzjSbz4C0TdKUyYdYs+tW3gGjNQSCvDiA2\n1KOw5dvtM35uyWM4pOVSN/lCqU6TTbI7ZeNfgcA1AoGAN7yO23L0v1FDF6dc9E/T\nUjMtL0fSPLDvpdiDrK6K+MMcuyQurOELIPqP+qc/viLyJWjtAmyP2wiyQfbGpykE\nEGM5E4QVy42Mq68kFgPJKMI0yMRw90JDnLUaqOqU21oWO8+7MYSpSQoc5JSJpCuc\nAlCcCikbaOM1QE8n53FQCMU=\n-----END PRIVATE KEY-----\n",
+  client_email: "sheetssync@godmindsetpodcast2020.iam.gserviceaccount.com",
+  client_id: "104438364389397140679",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/sheetssync%40godmindsetpodcast2020.iam.gserviceaccount.com",
+};
