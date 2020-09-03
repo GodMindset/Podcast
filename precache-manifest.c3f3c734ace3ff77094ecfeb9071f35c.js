afdc0b27b8e94a6ed0d11e2b81200d35d20c61a9
Andrei Lucaci <lucaciandrei@outlook.com>
2020-09-03 13:42:09 +0300

    Updates

---
+++ precache-manifest.c3f3c734ace3ff77094ecfeb9071f35c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe5860f977ac0661f2ee9d5974396c3",
+    "url": "/podcast/index.html"
+  },
+  {
+    "revision": "27a39dfd955174f9e163",
+    "url": "/podcast/static/css/main.39299768.chunk.css"
+  },
+  {
+    "revision": "9ba9350669cd5d17a0fd",
+    "url": "/podcast/static/js/2.bc7aed4a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/podcast/static/js/2.bc7aed4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a39dfd955174f9e163",
+    "url": "/podcast/static/js/main.03af970a.chunk.js"
+  },
+  {
+    "revision": "6a9b93b4f5a7fc09ee40",
+    "url": "/podcast/static/js/runtime-main.faef1caa.js"
+  }
+]);
